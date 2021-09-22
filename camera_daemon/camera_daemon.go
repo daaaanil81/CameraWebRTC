1a4cb6e8248ccd25112f0b259a918c737af7a68f
Daniil Petrov <danil.petrov.live@gmail.com>
2021-09-22 23:23:57 +0300

    go: Add server functional
    Signed-off-by: Daniil Petrov <danil.petrov.live@gmail.com>

--- camera_daemon/camera_daemon.go
+++ camera_daemon/camera_daemon.go
@@ -2,20 +2,61 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/net/websocket"
 )
 
+func loadPage(filename string) ([]byte, error) {
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	var database = "/etc/camera_server/static/database/database.txt"
+
+	var name, pass string
+
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+
+	var file, err = os.OpenFile(database, os.O_RDWR, 0755)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer file.Close()
+
+	fmt.Fscan(file, &name, &pass)
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if username != name || password != pass {
+		fmt.Fprintf(w, "Incorrect username or password entered\n")
+
+		return
+	}
+
+	p, err := loadPage("/etc/camera_server/static/stream.html")
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return
+	}
+
+	w.Write(p)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,11 +73,20 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+func WSClient(ws *websocket.Conn) {
+	var test string
+	websocket.Message.Receive(ws, &test)
+	time.Sleep(10 * time.Second)
+	fmt.Println(test)
+}
+
 func main() {
+
 	fileServer := http.FileServer(http.Dir("/etc/camera_server/static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/login", formHandler)
 	http.HandleFunc("/hello", helloHandler)
+	http.Handle("/ws", websocket.Handler(WSClient))
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/daaaanil81/CameraWebRTC
 
 go 1.15
+
+require golang.org/x/net v0.0.0-20210917221730-978cfadd31cf
